49f967869f46e51992d629abda9f43f723b4b967
Najma <najmathande@gmail.com>
2023-05-01 05:52:05 +0300

    done

---
+++ botarmy/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import BotsPage from "./containers/BotsPage"
+import "./App.css"
+import "./index.css"
+
+export default function App() {
+  return (
+    <div className="App">
+      <BotsPage />
+      
+    </div>
+  )
+}
